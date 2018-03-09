22dd30014a1f6e411fff409471f81a47162fad83
admin <admin@ahmeds-macbook-pro.local>
2018-03-09 22:56:35 +0200

    basic example

--- OptimizationLevels/ViewController.swift
+++ OptimizationLevels/ViewController.swift
@@ -7,12 +7,45 @@
 //
 
 import UIKit
+import SwiftSoup
+
+extension String {
+    var logified: String {
+        return "\(Date()): \(self)\n"
+    }
+}
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private weak var outputTextView: UITextView!
+    
+    private func test() {
+        outputTextView.text = "Test started.".logified
+        outputTextView.text.append("Will read HTML file into a String...".logified)
+        DispatchQueue.global(qos: .default).async {
+            let html = try! String(contentsOf: Bundle.main.url(forResource: "Architecture of Seattle - Wikipedia", withExtension: "htm")!)
+            DispatchQueue.main.async {
+                self.outputTextView.text.append("Will parse HTML String with SwiftSoup...".logified)
+            }
+            let document = try! SwiftSoup.parse(html)
+            
+            DispatchQueue.main.async {
+                self.outputTextView.text.append("Will extract text with SwiftSoup...".logified)
+            }
+            
+            let _ = try! document.text()
+            
+            DispatchQueue.main.async {
+                self.outputTextView.text.append("Done.".logified)
+            }
+        }
+    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        test()
     }
 
     override func didReceiveMemoryWarning() {
